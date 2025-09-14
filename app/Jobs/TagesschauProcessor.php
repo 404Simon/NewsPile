0454f8986cfa0f8057eabc9468c4e949dac3120f
404Simon <s.wi@mail.de>
2025-09-14 15:58:43 +0200

    fix

--- app/Jobs/TagesschauProcessor.php
+++ app/Jobs/TagesschauProcessor.php
@@ -3,13 +3,14 @@
 namespace App\Jobs;
 
 use App\Models\Article;
+use App\Models\NewsOutlet;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-use SimplePie;
+use SimplePie\SimplePie;
 
 class TagesschauProcessor implements ShouldQueue
 {
@@ -29,24 +30,28 @@ class TagesschauProcessor implements ShouldQueue
     public function handle(): void
     {
 
-                Log::info('TagesschauProcessor started');
-        echo "hi mom";
+        Log::info('TagesschauProcessor started');
         // SimplePie initialisieren
-        $pie = new SimplePie();
+        $pie = new SimplePie;
 
         // Cache-Verzeichnis setzen (muss existieren und beschreibbar sein)
         $cacheDir = storage_path('app/simplepie_cache');
-        if (!is_dir($cacheDir)) {
+        if (! is_dir($cacheDir)) {
             mkdir($cacheDir, 0755, true);
         }
         $pie->set_cache_location($cacheDir);
         $pie->set_cache_duration(3600); // 1 Stunde
         $pie->enable_cache(true);
 
-        $pie->set_feed_url('https://www.tagesschau.de/infoservices/alle-meldungen-100~rss2.xml');
+        $newsOutlet = NewsOutlet::firstOrCreate(
+            ['name' => 'Tagesschau'],
+            ['url' => 'https://www.tagesschau.de', 'rss_url' => 'https://www.tagesschau.de/infoservices/alle-meldungen-100~rss2.xml', 'b64_logo' => 'b64logohiertodo']
+        );
+
+        $pie->set_feed_url($newsOutlet->rss_url);
 
         if ($pie->init()) {
-            $items = $pie->get_items() ?? [];
+            $items = $pie->get_items();
             foreach ($items as $item) {
                 $title = $item->get_title();
                 $link = $item->get_link();
@@ -57,11 +62,6 @@ class TagesschauProcessor implements ShouldQueue
                     'title' => $title,
                     'link' => $link,
                 ]);
-
-                $newsOutlet = NewsOutlet::firstOrCreate(
-                    ['name' => 'Tagesschau'],
-                    ['url' => 'https://www.tagesschau.de']
-                );
                 // Beispiel: sichere Massenzuweisung (Article::$fillable muss gesetzt sein)
                 try {
                     Article::create([
@@ -72,14 +72,14 @@ class TagesschauProcessor implements ShouldQueue
                         'published_at' => $pubDate,
                     ]);
                 } catch (\Throwable $e) {
-                    Log::error('Fehler beim Erstellen des Artikels: ' . $e->getMessage(), [
+                    Log::error('Fehler beim Erstellen des Artikels: '.$e->getMessage(), [
                         'title' => $title,
                         'link' => $link,
                     ]);
                 }
             }
         } else {
-            Log::error('SimplePie init failed: ' . $pie->error());
+            Log::error('SimplePie init failed: '.$pie->error());
         }
     }
 }
