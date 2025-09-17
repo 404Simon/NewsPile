6dfb798f018379b6325d7a7daff79e9b302a7e77
404Simon <s.wi@mail.de>
2025-09-17 17:38:51 +0200

    refactor: remove modal and use flux inputs

--- app/Livewire/SearchProfileOverview.php
+++ app/Livewire/SearchProfileOverview.php
@@ -12,10 +12,6 @@ class SearchProfileOverview extends Component
 {
     use WithPagination;
 
-    public bool $showDeleteModal = false;
-
-    public ?SearchProfile $profileToDelete = null;
-
     #[Title('Search Profiles')]
     public function render(): View
     {
@@ -29,33 +25,9 @@ class SearchProfileOverview extends Component
         ]);
     }
 
-    public function confirmDelete(SearchProfile $profile): void
+    public function deleteProfile(SearchProfile $profile): void
     {
-        $this->profileToDelete = $profile;
-        $this->showDeleteModal = true;
-    }
-
-    public function delete(): void
-    {
-        if (! $this->profileToDelete) {
-            return;
-        }
-
-        $this->authorize('delete', $this->profileToDelete);
-
-        $this->profileToDelete->delete();
-
-        $this->resetModal();
-    }
-
-    public function cancelDelete(): void
-    {
-        $this->resetModal();
-    }
-
-    private function resetModal(): void
-    {
-        $this->profileToDelete = null;
-        $this->showDeleteModal = false;
+        $this->authorize('delete', $profile);
+        $profile->delete();
     }
 }
